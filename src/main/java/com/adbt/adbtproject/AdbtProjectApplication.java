24c237c4f5546cc07b6176b6a494ce40f072ac27
takikuba <jakub26@gmail.com>
2022-03-15 10:41:59 +0100

    Add swagger

--- src/main/java/com/adbt/adbtproject/AdbtProjectApplication.java
+++ src/main/java/com/adbt/adbtproject/AdbtProjectApplication.java
@@ -1,14 +1,7 @@
 package com.adbt.adbtproject;
 
-import com.adbt.adbtproject.entities.User;
-import com.adbt.adbtproject.repo.UserRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class AdbtProjectApplication {
--- src/main/java/com/adbt/adbtproject/WebSecurityConfig.java
+++ src/main/java/com/adbt/adbtproject/WebSecurityConfig.java
@@ -1,12 +1,17 @@
 package com.adbt.adbtproject;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.security.web.authentication.AuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
 import java.util.Collections;
