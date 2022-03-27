3497ad668ad727d81511ca46d7b47cc37e68aad8
DamianZamojski <damian.zam1f20@gmail.com>
2022-03-27 13:09:31 +0200

    .

--- src/main/java/com/adbt/adbtproject/controller/ItemGroupApi.java
+++ src/main/java/com/adbt/adbtproject/controller/ItemGroupApi.java
@@ -1,14 +1,16 @@
 package com.adbt.adbtproject.controller;
 
+import com.adbt.adbtproject.entities.ItemGroup;
+import com.adbt.adbtproject.entities.User;
+import com.adbt.adbtproject.entities.Warehouse;
 import com.adbt.adbtproject.repo.ItemGroupRepo;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -21,6 +23,32 @@ public class ItemGroupApi {
     @GetMapping("/{id}")
     public ResponseEntity<Integer> getItemCount(@PathVariable String id) {
         return new ResponseEntity<>(Optional.of(itemsRepo.findById(id).get().allAvailability()).orElse(0), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<HttpStatus> createItemGroup(@RequestBody ItemGroup itemGroup) {
+        itemsRepo.save(itemGroup);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteItemGroup(@PathVariable String id) {
+        itemsRepo.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ItemGroup>> getAll() {
+        return new ResponseEntity<>(itemsRepo.findAll(), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ItemGroup> updateItemGroup(@PathVariable String id, @RequestBody ItemGroup itemGroupRequest) {
+        ItemGroup itemGroup = itemsRepo.findById(id).get();
+
+        BeanUtils.copyProperties(itemGroupRequest, itemGroup);
+
+        return ResponseEntity.ok(itemsRepo.save(itemGroup));
     }
 
 }
